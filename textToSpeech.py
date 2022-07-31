fe8e85c58ff10fa947006f8b12499bc89a8660e5
Sourcery AI <>
2022-07-31 14:26:31 +0000

    'Refactored by Sourcery'

--- textToSpeech.py
+++ textToSpeech.py
@@ -9,19 +9,16 @@ client = boto3.client(
     region_name="ap-southeast-2"
 )
 
-textFile = open("TwinkleNurseryRhyme.txt","r")
-response = client.synthesize_speech(Text=textFile.read(), OutputFormat="mp3",
-                                        VoiceId="Nicole")
+with open("TwinkleNurseryRhyme.txt","r") as textFile:
+    response = client.synthesize_speech(Text=textFile.read(), OutputFormat="mp3",
+                                            VoiceId="Nicole")
 
 
-#print the json response
-pprint.pprint(response)
+    #print the json response
+    pprint.pprint(response)
 
-speechFile = open("NurseryRhyme.mp3","wb")
-speechFile.write(response['AudioStream'].read())
-speechFile.close()
-
-textFile.close()
+    with open("NurseryRhyme.mp3","wb") as speechFile:
+        speechFile.write(response['AudioStream'].read())
 
 
 
